efc38cd2aeeae4926511b62912a677824d2756ea
22Rupesh <rovertrupesh@gmail.com>
2026-01-06 21:55:24 +0530

    Fix: use environment-based API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,27 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api"
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return req;
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
+// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
