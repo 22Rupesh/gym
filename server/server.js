fe4149dd0c7b8305f88d854d1926b7eb85d1f3b9
22Rupesh <rovertrupesh@gmail.com>
2026-01-06 20:35:29 +0530

    Fix: downgrade express to stable v4

--- server/server.js
+++ server/server.js
@@ -39,12 +39,21 @@ const server = http.createServer(app);
 const Message = require("./models/Message");
 
 // attach socket.io
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"]
+//   }
+// });
+
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
+
 
 // socket logic
 io.on("connection", (socket) => {
